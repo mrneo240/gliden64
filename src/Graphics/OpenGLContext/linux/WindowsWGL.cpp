7d4f83c96638bf11651b37daf94fa4b051793b46
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2022-02-06 14:20:40 -0500

    fix: allow configuring SDL include path

--- src/Graphics/OpenGLContext/linux/WindowsWGL.cpp
+++ src/Graphics/OpenGLContext/linux/WindowsWGL.cpp
@@ -10,8 +10,13 @@
 #include <unistd.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#if defined(__MINGW32__) || defined(USE_SDL2_INCLUDE_PATH_SHORT)
+#include <SDL.h>
+#include <SDL_opengl.h>
+#else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
+#endif
 #include <GL/gl.h>
 
 static SDL_Window *_wind = NULL;
