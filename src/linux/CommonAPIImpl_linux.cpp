6dc0225ed440371ae637b455ab4f4cdaebd6e100
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2022-02-06 04:24:48 -0500

    fix: correct path handling on linux

--- src/linux/CommonAPIImpl_linux.cpp
+++ src/linux/CommonAPIImpl_linux.cpp
@@ -12,16 +12,7 @@ int PluginAPI::InitiateGFX(const GFX_INFO & _gfxInfo)
 
 void PluginAPI::FindPluginPath(wchar_t * _strPath)
 {
-#if 0
-	if (_strPath == NULL)
-		return;
-	::GetModuleFileNameW((HINSTANCE)&__ImageBase, _strPath, PLUGIN_PATH_SIZE);
-	std::wstring pluginPath(_strPath);
-	std::replace(pluginPath.begin(), pluginPath.end(), L'\\', L'/');
-	std::wstring::size_type pos = pluginPath.find_last_of(L"/");
-	wcscpy(_strPath, pluginPath.substr(0, pos).c_str());
-#endif
-	wcscpy(_strPath, L"./");
+	wcscpy(_strPath, L".");
 	return;
 }
 
